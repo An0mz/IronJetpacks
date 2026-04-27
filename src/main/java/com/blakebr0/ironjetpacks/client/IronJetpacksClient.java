6f7811501a695debc7d9b1405242cc46bdd87cb5
Anomz <58313106+An0mz@users.noreply.github.com>
2026-04-27 14:33:59 -0400

    Update IronJetpacksClient.java

--- src/main/java/com/blakebr0/ironjetpacks/client/IronJetpacksClient.java
+++ src/main/java/com/blakebr0/ironjetpacks/client/IronJetpacksClient.java
@@ -49,7 +49,7 @@ public class IronJetpacksClient {
 
                 @Override
                 public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, ItemStack itemStack, HumanoidRenderState humanoidRenderState, EquipmentSlot equipmentSlot, int i, HumanoidModel<HumanoidRenderState> humanoidModel) {
-                    int colorTint = jetpack.item.get().getColorTint(1);
+                    int colorTint = jetpack.item.get().getColorTint(0);
                     float r = (float) (colorTint >> 16 & 255) / 255.0F;
                     float g = (float) (colorTint >> 8 & 255) / 255.0F;
                     float b = (float) (colorTint & 255) / 255.0F;
